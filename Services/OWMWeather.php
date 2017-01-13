4a9333d2e446cf0e030399dcf7656a3d26e1547c
Stephane <stephane@sbnet.fr>
2017-01-13 17:38:58 +0100

    addes getForecastByCoord

--- Services/OWMWeather.php
+++ Services/OWMWeather.php
@@ -10,4 +10,21 @@ namespace  Sbnet\WeatherBundle\Services;
 
 class OWMWeather extends Weather
 {
+    private $apiUrl = "http://api.openweathermap.org/data/2.5/";
+    private $params = "units=metric&lang=fr";
+
+    private function makeUrl($adr)
+    {
+        $url = $this->apiUrl
+            .$adr
+            ."&APPID=".$this->getKey()
+            ."&".$this->params;
+        return $url;
+    }
+
+    public function getForecastByCoord($lat, $lon)
+    {
+        $adr = $this->makeUrl("forecast?lat=$lat&lon=$lon");
+        return $this->getJsonFromUrl($adr);
+    }
 }
